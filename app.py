17809dac00df72c217dbe86658e04c61c62c51a9
Debajyoti Roy <debajyotiroy21@gmail.com>
2024-02-06 11:02:31 +0530

    Environment Variable created.

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import pandas as pd
 
 load_dotenv()
 os.getenv("GOOGLE_API_KEY")
-genai.configure(api_key='AIzaSyDMPPvASvD28fUvAnjNny3OH8PuFa0PKl0')
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 
 def get_pdf_text(pdf_docs):
